9f9f97189491a9cb3b5aceb4924853114e209a2e
Celes-ovo <74448836+Celes-ovo@users.noreply.github.com>
2024-08-06 12:51:19 +0900

    Add files via upload

---
+++ main/main.py
@@ -0,0 +1,93 @@
+from txt_generate import textwrite
+from xml_generate import xml_writer
+
+import pandas as pd, os
+import sys
+
+from sm_img_proc import get_classes
+
+
+
+def __init__(self):
+    self.label_data = None
+    self.dsVOC = 'ceph_VOC' #'VOC'
+    self.year = '2007'
+    self.csv = None
+
+
+
+def main():
+    print(len(sys.argv))
+    if len(sys.argv) != 4:
+        print('[Error]')
+        exit()
+
+    csv_name = sys.argv[1]
+    dir = sys.argv[2]
+    target_dir = sys.argv[3]
+
+    # print(csv_name, dir, target_dir)
+    # return
+
+    
+    # parameters for file creation
+    dsVOC = 'ceph_VOC' #'VOC'
+    year = '2023'
+
+    # 이미지의 size에 따라 가변적으로 수정되어야 하는 parameter # celes
+    box_parm = 32
+
+    # landmark_name_fstr = '/mnt/d/MMMIL/Pnet/src/workspace/Preprocessing/Landmark pointer/landmark_46.csv'
+    landmark_name, _      = get_classes() # celes
+
+
+    # csv load
+    if csv_name == None:
+        csv_name = input('Type your csv name (ex: label_data.csv) : ')
+
+    # image dir
+    if dir == None:
+        dir = input('Type your image directory name : ')
+    if target_dir == None:
+        target_dir = input('Type your target dir name : ')
+
+
+    label_path = f'{csv_name}'
+
+    if os.path.exists(label_path)==True:
+        label_data = pd.read_csv(f'{csv_name}')
+
+    elif os.path.exists(label_path)==False:
+        label_data = pd.read_csv(f'{csv_name}')
+
+
+    print('\n >>> csv file has been loaded.\n\n ================================')
+
+
+
+    main_dir = f'{target_dir}/VOCdevkit/{dsVOC}{year}'
+
+    os.makedirs(main_dir + '/JPEGImages/', exist_ok=True)
+    os.makedirs(main_dir + '/Annotations/', exist_ok=True)
+    os.makedirs(main_dir + '/ImageSets/Main/', exist_ok=True)
+
+    print('[Log] Directory has been created.')
+
+
+    #image_shape_to_csv()
+    textwrite(target_dir, label_data, dsVOC, year)
+    xml_writer(target_dir, label_data, dsVOC, year, dir, main_dir + '/JPEGImages/',
+                landmark_name, box_parm=box_parm)
+
+
+    print('================================ \n\n >>> Done')
+
+
+
+    # getopt
+    # https://kaspyx.tistory.com/69
+    # os.path.join 사용해보기
+
+
+if __name__ == "__main__":
+    main()
---
+++ main/xml_generate.py
@@ -0,0 +1,108 @@
+import pandas as pd, numpy as np
+from xml.etree.ElementTree import Element, SubElement, ElementTree
+import cv2, os
+from tqdm import tqdm
+
+
+# label_data : csv file
+
+def xml_writer(target_dir, label_data, dsVOC, year, dir_src, save_dir, landmark_name, box_parm=8):
+    
+    len_file = len(label_data['ID'])
+
+    # jpg format, png format
+    img_dir = os.listdir(dir_src)
+        
+    for i in tqdm(range(len_file)):
+        root = Element("annotation")
+
+        element1 = Element("filename")
+        root.append(element1)
+        element1.text = label_data.iloc[i]['ID']
+
+        image_name = label_data.iloc[i]['ID']
+
+        ###
+        # img = cv2.imread(f'./{dir_src}/{image_name}.jpg', cv2.IMREAD_GRAYSCALE)
+        # target_path = f'{save_dir}/{image_name}.jpg'
+        
+        # img = cv2.imread(f'./{dir_src}/{image_name}.png', cv2.IMREAD_GRAYSCALE)
+
+        
+
+        # img = cv2.imread(f'./{dir_src}/{image_name}.png')
+        img = cv2.imread(f'{dir_src}/{img_dir[i]}')
+        
+        target_path = f'{save_dir}/{image_name}.png'
+
+        ##
+        # print(image_name)
+
+        cv2.imwrite(target_path, img[:, :, ::-1])
+
+
+        element2 = Element("size")
+        root.append(element2)
+
+        width = str(img.shape[1])
+        height = str(img.shape[0])
+        channel = str(img.shape[2])
+
+        # bbox size 조절
+        bbox_size = max(int(width)//box_parm, int(height)//box_parm)
+
+        
+        sub_element2 = SubElement(element2, "width")
+        sub_element2.text = width
+        sub_element2 = SubElement(element2, "height")
+        sub_element2.text = height
+        sub_element2 = SubElement(element2, "depth")
+        sub_element2.text = channel
+
+        
+
+        for j in range( len(landmark_name) ):
+            element3 = Element("object")
+            root.append(element3)
+            sub_element3 = SubElement(element3, "name")
+            sub_element3.text = landmark_name[j]
+            
+            x_loc = label_data.iloc[i][landmark_name[j]+'_x']
+            y_loc = label_data.iloc[i][landmark_name[j]+'_y']
+            
+            # idea : 1 이상, 1 이하일 경우 -> parameter=False 등으로 해서 True 여부 체크하는 기능 만드는 것 필요
+            # mode 등을 두어서 label data가 소숫점일 경우와 1 이상일 경우에 대해 각각 수행.
+            
+            # tmp = np.array([x_loc*float(width)-bbox_size, x_loc*float(width)+bbox_size, y_loc*float(width)-bbox_size, y_loc*float(width)+bbox_size]).astype(int)
+            try:
+                tmp = np.array([x_loc-bbox_size, x_loc+bbox_size, y_loc-bbox_size, y_loc+bbox_size]).astype(int)
+            
+            except:
+                tmp = np.array([-1, -1, -1, -1]).astype(int)
+
+
+            sub_element4 = SubElement(element3, "bndbox")
+
+            sub_element5 = SubElement(sub_element4, "xmin")
+            sub_element5.text = str(tmp[0])
+
+            sub_element6 = SubElement(sub_element4, "ymin")
+            sub_element6.text = str(tmp[2])
+
+            sub_element7 = SubElement(sub_element4, "xmax")
+            sub_element7.text = str(tmp[1])
+
+            sub_element8 = SubElement(sub_element4, "ymax")
+            sub_element8.text = str(tmp[3])
+        
+        
+        tree = ElementTree(root)
+        
+        #i_2 = '{0:04d}'.format(i)
+        i_2 = label_data['ID'][i]
+        fileName = f"{target_dir}/VOCdevkit/{dsVOC}{year}/Annotations/{i_2}.xml"
+        
+        with open(fileName, "wb") as file:
+            tree.write(file, encoding='utf-8', xml_declaration=True)
+
+    print('[Log] xml file has been created.')
